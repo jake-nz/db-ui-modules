301c4ce0deec3f1592f791701c480490f3c42304
Jake <jake@jake.nz>
2024-11-08 16:36:01 +1000

    Add prettier

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+// @ts-check
+/** @type {import("@ianvs/prettier-plugin-sort-imports").PrettierConfig} */
+module.exports = {
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+}
