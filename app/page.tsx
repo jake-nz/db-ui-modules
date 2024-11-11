37e8edd6728fee5c56acfbf1750ced2a04781d07
Jake <jake@jake.nz>
2024-11-12 08:59:04 +1000

    Make landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,80 @@
+import { PlusOutlined } from '@ant-design/icons'
+import { Button, Card, Col, Row, Space, Statistic } from 'antd'
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default function Home() {
-  return <div></div>
+  return (
+    <Space
+      direction="vertical"
+      size="large"
+      style={{ width: '100%', alignItems: 'center' }}
+    >
+      <Image
+        src={'/images/logo-dark.png'}
+        width={1609 / 7}
+        height={1154 / 7}
+        alt="logo"
+      />
+      <Space direction="vertical" size="middle" style={{ width: 500 }}>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Card bordered={false}>
+              <Statistic
+                title="Outplants this month"
+                value={2145}
+                precision={0}
+                // valueStyle={{ color: '#3f8600' }}
+                // prefix={<ArrowUpOutlined />}
+                // suffix="%"
+              />
+            </Card>
+          </Col>
+          <Col span={12}>
+            <Card bordered={false}>
+              <Statistic
+                title="Outplants this year"
+                value={105392}
+                precision={0}
+                // valueStyle={{ color: '#cf1322' }}
+                // prefix={<ArrowDownOutlined />}
+                // suffix="%"
+              />
+            </Card>
+          </Col>
+        </Row>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Card bordered={false}>
+              <Statistic
+                title="Number of species"
+                value={71}
+                precision={0}
+                // valueStyle={{ color: '#3f8600' }}
+                // prefix={<ArrowUpOutlined />}
+                // suffix="%"
+              />
+            </Card>
+          </Col>
+          <Col span={12}>
+            <Card bordered={false}>
+              <Statistic
+                title="Planting hours"
+                value={1840}
+                precision={0}
+                // valueStyle={{ color: '#cf1322' }}
+                // prefix={<ArrowDownOutlined />}
+                // suffix="%"
+              />
+            </Card>
+          </Col>
+        </Row>
+      </Space>
+      <Link href="/outplants/create">
+        <Button type="primary" icon={<PlusOutlined />}>
+          Enter Outplants
+        </Button>
+      </Link>
+    </Space>
+  )
 }
