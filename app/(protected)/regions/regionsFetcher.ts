ab92136d396d1aed060e1ce10423536482076752
Jake <jake@jake.nz>
2025-04-01 16:49:10 +1000

    RegionSelect component

--- app/(protected)/regions/regionsFetcher.ts
+++ app/(protected)/regions/regionsFetcher.ts
@@ -1,6 +1,5 @@
 'use server'
 import { database } from '@/services/database/database'
-// import { InsertObject, UpdateObject } from 'kysely'
 import { ListQuery, paginate } from 'snaks'
 
 export const regionsFetcher = async ({ page, filters, sorter }: ListQuery) => {
@@ -10,44 +9,13 @@ export const regionsFetcher = async ({ page, filters, sorter }: ListQuery) => {
     .leftJoin('outplantDays', 'outplantDays.operator', 'operators.id')
     .leftJoin('outplants', 'outplants.day', 'outplantDays.id')
     .select(sb => [
+      'regions.id',
       'regions.name',
       'color',
       sb.fn.sum('count').as('totalOutplants')
     ])
-    .groupBy(['regions.name', 'color'])
+    .groupBy(['regions.id', 'regions.name', 'color'])
     .orderBy('name asc')
 
   return paginate(query, page, 100).execute()
 }
-
-// export const eventFetcher = async (id: string) => {
-//   const event = database
-//     .selectFrom('events')
-//     .selectAll()
-//     .where('id', '=', id)
-//     .executeTakeFirstOrThrow()
-//   const cash = database
-//     .selectFrom('cash')
-//     .selectAll()
-//     .where('eventId', '=', id)
-//     .execute()
-//   return { event: await event, cash: await cash }
-// }
-
-// export const eventCreate = async (data: InsertObject<DB, 'events'>) =>
-//   await database
-//     .insertInto('events')
-//     .values(data)
-//     .returningAll()
-//     .executeTakeFirstOrThrow()
-
-// export const eventUpdate = async (
-//   id: string,
-//   data: UpdateObject<DB, 'events'>
-// ) => {
-//   await database
-//     .updateTable('events')
-//     .set(data)
-//     .where('id', '=', id)
-//     .executeTakeFirstOrThrow()
-// }
