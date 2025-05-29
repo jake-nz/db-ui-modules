1e36c4c024aa8f6744e23f0d97bde110b7618887
Jake <jake@jake.nz>
2025-05-29 21:26:48 +1000

    add selectFilter

---
+++ components/selectFilter.tsx
@@ -0,0 +1,30 @@
+import { Select } from 'antd'
+import { FilterDropdownProps } from 'antd/es/table/interface'
+import { ComponentProps, ReactNode } from 'react'
+
+interface SelectFilterProps extends ComponentProps<typeof Select> {
+  includeArchived?: boolean
+}
+
+type SelectComponent = (props: SelectFilterProps) => ReactNode
+
+export const selectFilter = (Component: SelectComponent) => {
+  const SelectFilter = ({ setSelectedKeys, selectedKeys, confirm, clearFilters }: FilterDropdownProps) => (
+    <div style={{ padding: 8 }} onKeyDown={e => e.stopPropagation()}>
+      <Component
+        value={selectedKeys}
+        placeholder="Filter"
+        popupMatchSelectWidth={false}
+        allowClear
+        includeArchived
+        onClear={() => clearFilters && clearFilters()}
+        onChange={v => {
+          setSelectedKeys([v as string | number])
+          confirm({ closeDropdown: true })
+        }}
+      />
+    </div>
+  )
+
+  return SelectFilter
+}
