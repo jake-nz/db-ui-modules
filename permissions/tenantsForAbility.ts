422b9e7f135d5d98aa76f02c03fc6206bd16482c
Jake <jake@jake.nz>
2025-06-05 13:28:23 +1000

    fix(permissions): allow different tenantIdProp

--- permissions/tenantsForAbility.ts
+++ permissions/tenantsForAbility.ts
@@ -1,13 +1,25 @@
 import { type MongoAbility } from '@casl/ability'
+import { User } from 'next-auth'
+
+type TenantType = Exclude<User['roles'][number]['tenantId'], null>
 
 /**
  * Return all the tenants for which the ability can perforn `action` on `subject`
  */
-export function tenantsForAbility<Action extends string, Subject extends string>(ability: MongoAbility, action: Action, subject: Subject) {
-  const tenantIds = []
+export function tenantsForAbility<Action extends string, Subject extends string>(
+  ability: { rulesFor: MongoAbility['rulesFor'] },
+  action: Action,
+  subject: Subject,
+  tenantIdProp: string
+) {
+  const tenantIds = new Set<TenantType>()
   for (const rule of ability.rulesFor(action, subject)) {
-    if (!rule.conditions?.tenantId) return null
-    tenantIds.push(rule.conditions.tenantId as number)
+    if (!rule.conditions?.[tenantIdProp]) return null
+    if (rule.inverted) {
+      tenantIds.delete(rule.conditions[tenantIdProp] as TenantType)
+    } else {
+      tenantIds.add(rule.conditions[tenantIdProp] as TenantType)
+    }
   }
-  return tenantIds
+  return Array.from(tenantIds)
 }
