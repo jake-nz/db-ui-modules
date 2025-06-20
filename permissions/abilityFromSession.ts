bbdd242d2d88ac330a5954ec74ddc55835f00c8d
Jake <jake@jake.nz>
2025-06-20 14:39:08 +1000

    Facility switcher

--- permissions/abilityFromSession.ts
+++ permissions/abilityFromSession.ts
@@ -10,23 +10,6 @@ export const emptyAbility = extendAbility(new PureAbility() as AppAbility<Permis
 
 export const abilityFromSession = (session: Session | null) => {
   if (!session) return emptyAbility
-
   const roles = session.user?.roles
-
-  // const rolesOverride = [
-  //   {
-  //     role: 'volunteer',
-  //     tenantId: 'VWuPP17nxyXpL4'
-  //   },
-  //   {
-  //     role: 'volunteer',
-  //     tenantId: 'yL13WxfRYUfCKE'
-  //   },
-  //   {
-  //     role: 'volunteer',
-  //     tenantId: '7XpuR3uxtdIHkJ'
-  //   }
-  // ]
-
   return defineAbilityForRoles<PermissionsTypes>(permissions, roles)
 }
