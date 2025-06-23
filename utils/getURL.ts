814d9cfd398bbe0bff21aa5328326f67f0f7e6cf
Jake <jake@jake.nz>
2025-06-23 16:58:31 +1000

    fix: incorrect directory spelling and double extension

--- utils/getURL.ts
+++ utils/getURL.ts
@@ -1,5 +1,6 @@
 export const getURL = () => {
-  let url = process?.env?.NEXT_PUBLIC_SITE_URL ?? process?.env?.NEXT_PUBLIC_VERCEL_URL ?? `http://${window.location.host}`
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? process?.env?.NEXT_PUBLIC_VERCEL_URL ?? `http://${window.location.host}`
   // Make sure to include `https://` when not localhost.
   url = url.startsWith('http') ? url : `https://${url}`
   // Make sure to including trailing `/`.
