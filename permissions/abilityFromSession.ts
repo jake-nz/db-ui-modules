c9f4e89416be0bcc10dbdfe3059cbaa30f2fed8b
Jake <jake@jake.nz>
2025-07-23 09:35:39 +0900

    fix permissions path

--- permissions/abilityFromSession.ts
+++ permissions/abilityFromSession.ts
@@ -1,4 +1,4 @@
-import { permissions, PermissionsTypes } from '@/services/permissions/permissions'
+import { permissions, PermissionsTypes } from '@/services/auth/permissions'
 import { PureAbility } from '@casl/ability'
 import { Session } from 'next-auth'
 import { defineAbilityForRoles } from './defineAbilityForRoles'
@@ -11,5 +11,6 @@ export const emptyAbility = extendAbility(new PureAbility() as AppAbility<Permis
 export const abilityFromSession = (session: Session | null) => {
   if (!session) return emptyAbility
   const roles = session.user?.roles
+  console.log(roles)
   return defineAbilityForRoles<PermissionsTypes>(permissions, roles, session.user.id)
 }
