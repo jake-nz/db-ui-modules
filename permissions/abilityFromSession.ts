8d65c163501a23c6e4c1f9bb8cb6c2a126be4566
Jake <jake@jake.nz>
2025-08-14 08:08:13 +1000

    tidy: Remove console log

--- permissions/abilityFromSession.ts
+++ permissions/abilityFromSession.ts
@@ -11,6 +11,5 @@ export const emptyAbility = extendAbility(new PureAbility() as AppAbility<Permis
 export const abilityFromSession = (session: Session | null) => {
   if (!session) return emptyAbility
   const roles = session.user?.roles
-  console.log(roles)
   return defineAbilityForRoles<PermissionsTypes>(permissions, roles, session.user.id)
 }
