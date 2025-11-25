f10341142ea37d4b99a3d0008ab3311d803bb856
Jake <jake@jake.nz>
2025-11-25 16:32:23 +1000

    feat(Datetime): Handle dates in epoch seconds

--- components/Datetime.tsx
+++ components/Datetime.tsx
@@ -1,24 +1,51 @@
 import { Typography } from 'antd'
+import { TextProps } from 'antd/es/typography/Text'
 
 const { Text } = Typography
 
 const dateFormatter = new Intl.DateTimeFormat('en-AU', {
   dateStyle: 'medium',
 })
-export const dateFormat = (date: null | undefined | Date | string | number, fallback = '') => {
+
+const datetimeFormatter = new Intl.DateTimeFormat('en-AU', {
+  dateStyle: 'medium',
+  timeStyle: 'short'
+})
+
+export const normalizeDate = (date: Date | string | number) => {
+  if (date instanceof Date) return date
+
+  // Handle dates in epoch seconds
+  if (typeof date === 'number' && date < 10000000000) date = date * 1000
+
+  return new Date(date)
+}
+
+export const dateFormat = (date: null | undefined | Date | string | number, showTime = false, fallback = '') => {
   if (!date) return fallback
   try {
-    if (date instanceof Date) return dateFormatter.format(date)
-
-    // Handle dates in epoch seconds
-    if (typeof date === 'number' && date < 10000000000) date = date * 1000
-
-    return dateFormatter.format(new Date(date))
+    const normalizedDate = normalizeDate(date)
+    const formatter = showTime ? datetimeFormatter : dateFormatter
+    return formatter.format(normalizedDate)
   } catch {
     return date.toString()
   }
 }
 
+<<<<<<< HEAD
 export const Datetime = ({ children }: { children: Date | string | number | undefined | null }) => (
   <Text ellipsis>{dateFormat(children)}</Text>
+=======
+export const Datetime = ({
+  children,
+  showTime = false,
+  ...props
+}: {
+  children: Date | string | number | undefined | null
+  showTime?: boolean
+} & Omit<TextProps, 'children'>) => (
+  <Text ellipsis {...props}>
+    {dateFormat(children, showTime)}
+  </Text>
+>>>>>>> 626f84b (feat(Datetime): Handle dates in epoch seconds)
 )
